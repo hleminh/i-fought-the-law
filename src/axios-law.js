5a4220499b8b69e073d14be76c9ebb0a1ba8862f
yungwiche <le.minhhoang@outlook.com>
2018-05-17 01:12:07 +0700

    Changed axios port for laws.

--- src/axios-law.js
+++ src/axios-law.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:4000",
+  baseURL: "http://localhost:4100",
   headers: {
     Accept: "application/json",
     "Content-Type": "application/json"
